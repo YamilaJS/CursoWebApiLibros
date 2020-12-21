94f1f4999b87dc6267eee7705123b9fd8e6f7757
Yamila <yami.prog23@gmail.com>
2020-12-21 16:32:08 -0300

    Added NewtonsoftJson in startup.cs

--- CursoWebApiLibros/Startup.cs
+++ CursoWebApiLibros/Startup.cs
@@ -29,10 +29,10 @@ namespace CursoWebApiLibros
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers().AddNewtonsoftJson(x => { x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore; });
             services.AddControllers();
             services.AddDbContext<ApplicationDbContext>(options => 
             options.UseSqlServer(Configuration.GetConnectionString("DefaultConecctionString")));
-            //services.AddControllers().AddNewtonsoftJson(x => { x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
